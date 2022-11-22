26b21fe834cca3e61fd9b940ff8b1350d0f76222
s183558 <s183558@student.dtu.dk>
2022-11-22 23:31:00 +0100

    Update main.py
    Made it so the environment returns a state vector of shape (96,96,1) instead of (96,96).
    Added comments to everything.
    made and if-statement that saves the model parameters if the avg score increases.
    Plottet the reward at each episode

--- source/main.py
+++ source/main.py
@@ -1,20 +1,22 @@
 import numpy as np
 import gym
 from gym import wrappers
-import matplotlib.pyplot as plt
 
 from agent import DDPGAgent
+from utils import plot_learning_curve
 
 ######################
 #     Parameters     #
 ######################
-gamma = 0.9
-tau = 0.01
-lr_P, lr_Q = 0.0001, 0.0001
+gamma = 0.99
+tau = 0.005
+lr_mu, lr_Q = 0.0001, 0.0002
 episodes = 10
 batch_size = 32
 step_size = batch_size * 4
-verbose_freq = 10
+verbose_freq = 50
+
+figure_file = 'plots/CarRacing_rewards.png'
 
 ######################
 #     Environment    #
@@ -22,42 +24,67 @@ verbose_freq = 10
 env = gym.make('CarRacing-v2', new_step_api=True) # Create environment
 #env = Recorder(env, "./video", auto_release = False) # To display environment in Colab
 env.reset(options={"randomize": False}, seed = 42) # Reset environment
-env = wrappers.GrayScaleObservation(env)
+env = wrappers.GrayScaleObservation(env, keep_dim = True)
 
 obs_shape = env.observation_space.shape
 
 # Agent
-agent = DDPGAgent(gamma, tau, lr_P, lr_Q)
+agent = DDPGAgent(gamma = gamma, tau = tau,lr_mu = lr_mu,
+                  lr_Q = lr_Q, env = env, batch_size = batch_size)
+
+print(f'Online {agent.critic}')
+print(f'Online {agent.actor}')
 
 # Training
 rewards = []
+best_score = -5000
+
 
 for e in range(episodes):
 
-    state = env.reset()   # (96, 96) image
-    reward_episode = 0
+    state = env.reset()   # (96, 96, 1) image
+    score = 0
 
     for step in range(step_size):
-        #plt.imshow(state, cmap="gray")
+        # Sample an action from our actor network
         action = agent.getAction(state)[0]
         
+        # Take the action in our current state
         state_next, reward, done, done2, _ = env.step(action)
-        agent.memory.push(state, action, reward, state_next, np.array([done, done2]).any())
-
+        
+        # Add the reward from the step to our score
+        score += reward
+        
+        # Throm all our variables in the memory
+        agent.remember(state, action, reward, state_next, np.array([done, done2]).any())
+        
+        # When we have enough state-action pairs, we can update our online nets
         if len(agent.memory) > batch_size:
-            agent.update(batch_size)
-            agent.update(batch_size)
-            break
+            agent.update()
 
+        # The next state, is now our current state.
         state = state_next
-        reward_episode += reward
-
+        
+        # if the environment terminates before the step_size, we break
         if np.array([done, done2]).any():
-            print("episode: {:2}, reward: {:.2f}, average _reward: {}".format(
-                    e, reward_episode, np.mean(rewards)))
+            print(f'Terminated at:\nEpisode: {e+1}, reward: {score:.1f}, avg_reward: {np.mean(rewards[-100:]):.1f}')
             break
-        if (step+1) % verbose_freq == 0:
-            print(f'Step {step} done!')
-    print(f'Episode {e+1} done!')
-    rewards.append(reward_episode)
-    break
+        
+        # How far into the episode we are 
+        if (step+1) % verbose_freq == 0: print(f'Step {step+1} done!')
+            
+    # After each episode we store the score        
+    rewards.append(score)
+    
+    # The avg score of the last 100 episodes
+    avg_score = np.mean(rewards[-100:])
+    
+    if avg_score > best_score:
+        best_score = avg_score
+        agent.save_models()
+    
+    print(f'Episode {e+1}, score: {score:.1f}, avg_score: {avg_score:.1f}')
+
+
+x = [i + 1 for i in range(episodes)]
+plot_learning_curve(x, rewards, figure_file)
