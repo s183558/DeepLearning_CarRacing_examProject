ffb296901a78a6c28e1afcdad0b16f3775d8869b
s183558 <s183558@student.dtu.dk>
2022-12-03 16:17:14 +0100

    Implemented WandB, made it render and made it stop after x tries of negative reward

--- source/main.py
+++ source/main.py
@@ -3,6 +3,8 @@ drive_path = ''
 import numpy as np
 import gym
 from gym import wrappers
+import wandb
+#wandb.login()
 
 from agent import DDPGAgent
 from utils import plot_learning_curve
@@ -13,13 +15,17 @@ from utils import plot_learning_curve
 gamma = 0.99
 tau = 0.005
 lr_mu, lr_Q = 0.0001, 0.0002
-episodes = 200
+episodes = 10
 batch_size = 500
 step_size = 1000
 verbose_freq = 50
 
-
 figure_file = 'plots/CarRacing_rewards.png'
+
+do_wandb = False
+
+if do_wandb:
+    wandb_client = wandb.init(project="DL_CarRacing2")
 
 ######################
 #     Environment    #
@@ -28,17 +34,33 @@ env = gym.make('CarRacing-v2', new_step_api=True) # Create environment
 env.reset(options={"randomize": False}, seed = 42) # Reset environment
 env = wrappers.GrayScaleObservation(env, keep_dim = True)
 
-# Agent
+
+######################
+#       Agent        #
+######################
 agent = DDPGAgent(gamma = gamma, tau = tau,lr_mu = lr_mu,
                   lr_Q = lr_Q, env = env, batch_size = batch_size)
 
 print(f'Online {agent.critic}')
 print(f'Online {agent.actor}')
 
-# Training
+#######################
+# Training parameters #
+#######################
 rewards = []
-best_score = -5000
+best_score = 0
+no_reward_counter = 0
 
+# Wandb variables
+actor_loss = [[],[]]
+critic_loss = [[],[]]
+metrics = []
+metric_counter_step = 0
+metric_counter_model = 0
+
+#######################
+#      Main loop      #
+#######################
 
 for e in range(episodes):
 
@@ -48,17 +70,35 @@ for e in range(episodes):
     for step in range(step_size):
         # Sample an action from our actor network
         action = agent.getAction(state)[0]
+       
         
         # Take the action in our current state
         state_next, reward, done, done2, _ = env.step(action)
         
+        env.render()
+        
         # If the agent gives some gas it get a bonus reward
         bonus_reward = 0
         if action[1] > 0.1:
-            bonus_reward = 0.5 + action[1] #abs(reward *0.5)
-            
+            bonus_reward = 0.05 #0.5 + action[1] #abs(reward *0.5)
+        
+        # Position of the car
+        x = env.car.hull.position[0]
+        y = env.car.hull.position[1]
+
+        if do_wandb:
+            metrics.append({"reward"   : reward,
+                            "bonus_reward" : bonus_reward,
+                            "Steering" : action[0],
+                            "Gas"      : action[1],
+                            "Breaking" : action[2],
+                            "x pos"    : x,
+                            "y pos"    : y,
+                           })
+
         # Add the reward from the step to our score
-        score += reward + bonus_reward
+        reward += bonus_reward
+        score += reward
         
         # Throw all our variables in the memory
         agent.remember(state, action, reward, state_next, np.array([done, done2]).any())
@@ -66,15 +106,32 @@ for e in range(episodes):
 
         # When we have enough state-action pairs, we can update our online nets
         if len(agent.small_memory) == batch_size:
-            print(f'Updating the networks...')
+            print('Updating the networks...')
             # Update network with small buffer for recency
-            agent.update(recency_buffer = True)
+            smallB_metrics = agent.update(recency_buffer = True)
             
             # Move small buffer to big, and reset small buffer
             agent.move_small_buffer_to_big()
             
             # Update network with big buffer
-            agent.update(recency_buffer = False)
+            bigB_metrics = agent.update(recency_buffer = False)
+            
+            if do_wandb:
+                for i in range(batch_size):
+                    metrics[i].update({
+                             "Q-val (short term)": smallB_metrics[2][i],
+                             "y_i (short term)"  : smallB_metrics[3][i],
+                             "Q-val (long term)" : bigB_metrics[2][i],
+                             "y_i (long term)"   : bigB_metrics[3][i]})
+                    wandb_client.log(metrics[i])
+                metrics = []
+            
+            critic_loss[0].append(smallB_metrics[0])
+            critic_loss[1].append(bigB_metrics[0])
+            actor_loss[0].append(smallB_metrics[1])
+            actor_loss[1].append(bigB_metrics[1])
+            
+            
             
 
         # The next state, is now our current state.
@@ -85,8 +142,19 @@ for e in range(episodes):
         
         # if the environment terminates before the step_size, we break
         if np.array([done, done2]).any():
-            print(f'## ## Terminated at:')
+            print('## ## Terminated at:')
             break
+        
+        # If we dont get a positive reward in 200 steps, we reset
+        
+        if reward < 0:
+            no_reward_counter += 1
+            if no_reward_counter > 400:
+                print('No positive reward in 200 steps; RESET')
+                break
+        else:
+            no_reward_counter = 0
+            
         
     # After each episode we store the score        
     rewards.append(score)
@@ -104,8 +172,23 @@ for e in range(episodes):
     if avg_score > best_score:
         best_score = avg_score
         agent.save_models(drive_dir = drive_path)
-    
-    
+
+if do_wandb:
+    wandb_client.log(
+            {"actor_loss" : wandb.plot.line_series(
+                           xs = range(len(actor_loss)), 
+                           ys = actor_loss,
+                           keys = ["Short term", "long term"],
+                           title = "Actor loss",
+                           xname = "Step")})
+    wandb_client.log(
+            {"critic_loss" : wandb.plot.line_series(
+                           xs = range(len(critic_loss)), 
+                           ys = critic_loss,
+                           keys = ["Short term", "long term"],
+                           title = "Critic loss",
+                           xname = "Step")})
+    wandb_client.finish()
 
 # Save the final paramters of the model
 agent.save_models(suffix = '_final', drive_dir = drive_path)
